653aeb55778cda71b793e4a7dbf5c1a8074eee9c
Joel Soderstrom <joel.soderstrom@gmail.com>
2015-05-31 21:40:38 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Demo implementation of the Fortex JS engine
+
+## Run locally
+
+    npm install
+    bower install
+    gulp serve
+
+## The codez
+
+All logic is in `src/app/main/main.controller.js` and its corresponding view `src/app/main/main.html`.
+
+
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,100 @@
+'use strict';
+
+angular.module('fortexDemo').controller('MainCtrl', function ($scope, $timeout) {
+  
+  FX.debugMode = true;
+
+  $scope.stack = [];
+  $scope.invocation = {};
+  $scope.errand = null;
+  $scope.invoke = invoke;
+  $scope.back = back;
+
+  init();
+
+
+  function init() {
+    // Create errand based on delivery site data and push view model
+    $scope.errand = FX.errandExistingSite.create(devSite); 
+    push($scope.errand.getViewModel());
+  }
+
+  // `invoke` is the main function to drive the js engine. Any user action
+  // that the engine needs to be awared of is pushed using invoke.
+  // This could be when selecting and option or when the contact form is
+  // submitted
+  function invoke(o) {
+    $scope.invocation = o;
+
+    // Invoke returns a new view model where `actionType`
+    // decides which action to take
+    var viewModel = $scope.errand.invoke(o);
+
+    if(viewModel.actionType === 'open') {
+      handleOpen(viewModel);
+    }
+    if(viewModel.actionType === 'refresh') {
+      handleRefresh(viewModel);
+    }
+    if(viewModel.actionType === 'alert') {
+      handleAlert(viewModel);
+    }
+    if(viewModel.actionType === 'prompt') {
+      handlePrompt(viewModel);
+    }
+  }
+
+
+  ////////////////////////////////////
+  
+  // Action handlers
+  
+  function handleRefresh(o) {
+    if($scope.viewModel && $scope.viewModel.view !== o.view) {
+      console.warn('Refreshing view that is not active', o.view);
+    }  
+    $scope.viewModel = o;
+  }
+
+  function handleOpen(o) {
+    push(o);
+  }
+
+  function handleAlert(o) {
+    if(o.alert) {
+      alert(o.alert);
+    }
+  }
+
+  function handlePrompt(o) {
+    // Note: timeout just to see invocation the log pane
+    $timeout(function() { 
+      // Use text for buttons from object here in apps
+      invoke(confirm(o.text) ? o.yes : o.no);
+    });
+  }
+
+
+  ////////////////////////////////////
+
+  function back() {
+    $scope.errand.getViewModel('root');
+    pop();
+  }
+
+  
+  ////////////////////////////////////
+
+
+  function pop() {
+    $scope.stack.pop();
+    $scope.viewModel = $scope.stack[$scope.stack.length-1];
+    $scope.viewModel = $scope.errand.getViewModel();
+  }
+
+  function push(viewModel) {
+    $scope.stack.push(viewModel);
+    $scope.viewModel = viewModel;
+  }
+
+});
